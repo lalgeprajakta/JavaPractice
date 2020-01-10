f94091ad4855bbd7b25aa62386671b4a4d5d6b98
Prajakta <48818754+lalgeprajakta@users.noreply.github.com>
2020-01-10 11:11:52 +0530

    Add files via upload

---
+++ constructor/ConstuctorDemo.java
@@ -0,0 +1,13 @@
+package constructor;
+
+public class ConstuctorDemo {
+
+	public static void main(String[] args) {
+		
+		MyClass c1 = new MyClass();
+		MyClass c2 = new MyClass();
+		System.out.println(c1.num + " " + c2.num);
+		
+	}
+
+}
---
+++ constructor/MyClass.java
@@ -0,0 +1,9 @@
+package constructor;
+
+public class MyClass {
+
+	int num;
+	MyClass(){
+		num = 500;
+	}
+}
